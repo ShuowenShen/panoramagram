71f5813bd4f0a88290096b81528681c27971fa40
ShuowenShen <shenshuowen1234@163.com>
2014-11-26 23:51:26 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ using namespace cv;
 
 
 /** Global variables */
-String face_cascade_name = "/Users/sysc1234/macports/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml";
-Mat para = imread("/Users/sysc1234/Desktop/008.jpg",CV_LOAD_IMAGE_COLOR);  //path to image
+String face_cascade_name = "haarcascade_frontalface_alt.xml";
+Mat para = imread("argv[1]",CV_LOAD_IMAGE_COLOR);  //path to image
 CascadeClassifier face_cascade;
 string window_name = "Capture - Face detection";
 string window_name2 = "Para";
